10d4f47f3dcc0bab47ac58a29d292927c9ca89f0
Drubo Roy Bishwas <doctorhackan@gmail.com>
2025-12-02 21:26:06 +0600

    connected database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const connectDB = require('./src/configs/db');
+require('dotenv').config();
 
 
 const app = express();
+
+
+app.use(express.json());
+
+const port = process.env.PORT;
 
 
 app.get('/',(req,res)=>{
@@ -9,6 +16,14 @@ app.get('/',(req,res)=>{
 })
 
 
-app.listen(4001, ()=>{
-    console.log(`Listening at port 4001`);
+connectDB()
+.then(()=>{
+    app.listen(port, ()=>{
+        console.log(`Listening at port ${port}`);
+    });
+})
+.catch((err)=>{
+    console.log("Could not Connect to DB");
+    console.log("Error :",err.message);
 });
+
